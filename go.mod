2460e3b9024cb52db8aae1ce11f48bc2c26bbaa2
Ben Fickes <bfickes@pivotal.io>
2018-11-06 16:32:02 -0500

    WIP
    Co-authored-by: Danny Joyce <djoyce@pivotal.io>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cloudfoundry/nodejs-buildpack
 require (
 	github.com/Masterminds/semver v1.4.2 // indirect
 	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/buildpack/lifecycle v0.0.0-20181031025713-aec58da4a3fa // indirect
 	github.com/cloudfoundry/libbuildpack v0.0.0-20181102183835-772a83a22345
 	github.com/elazarl/goproxy v0.0.0-20181003060214-f58a169a71a5 // indirect
 	github.com/golang/mock v1.1.1
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,22 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
+GOOS=linux go build -ldflags="-s -w" -o bin/v3-detector github.com/buildpack/lifecycle/cmd/detector
+GOOS=linux go build -ldflags="-s -w" -o bin/v3-builder github.com/buildpack/lifecycle/cmd/builder
+
+mkdir -p cnbs/org.cloudfoundry.buildpacks.nodejs/0.0.1
+pushd cnbs/org.cloudfoundry.buildpacks.nodejs/0.0.1
+    wget https://github.com/cloudfoundry/nodejs-cnb/releases/download/v0.0.1-alpha/nodejs-cnb.tgz
+    tar xzvf nodejs-cnb.tgz
+    rm nodejs-cnb.tgz
+popd
+
+mkdir -p cnbs/org.cloudfoundry.buildpacks.npm/0.0.1
+pushd cnbs/org.cloudfoundry.buildpacks.npm/0.0.1
+    wget https://github.com/cloudfoundry/npm-cnb/releases/download/v0.0.1-alpha/npm-cnb.tgz
+    tar xzvf npm-cnb.tgz
+    rm npm-cnb.tgz
+popd
+
 GOOS=linux go build -ldflags="-s -w" -o bin/supply ./src/nodejs/supply/cli
 GOOS=linux go build -ldflags="-s -w" -o bin/finalize ./src/nodejs/finalize/cli
